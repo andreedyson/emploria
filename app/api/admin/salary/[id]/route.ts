4859c1c75cced839903073544e208bffad3ee15f
Dinann <notxurd46@gmail.com>
2025-05-31 20:15:50 +0700

    feat(api): add update status api route

---
+++ app/api/admin/salary/[id]/route.ts
@@ -0,0 +1,35 @@
+import prisma from "@/lib/db";
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+
+export async function PATCH(
+  req: NextRequest,
+  props: { params: Promise<{ id: string }> },
+) {
+  const params = await props.params;
+  const { status } = await req.json();
+
+  try {
+    const statusSchema = z.enum(["PAID", "UNPAID"]);
+    const parse = statusSchema.safeParse(status);
+    if (!parse.success) {
+      return NextResponse.json({ message: "Invalid status" }, { status: 400 });
+    }
+
+    const updatedSalary = await prisma.salary.update({
+      where: { id: params.id },
+      data: { status },
+    });
+
+    return NextResponse.json(
+      { message: "Salary status updated successfully", data: updatedSalary },
+      { status: 200 },
+    );
+  } catch (error) {
+    console.error("[UPDATE_SALARY_STATUS_ERROR]", error);
+    return NextResponse.json(
+      { message: "Internal server error" },
+      { status: 500 },
+    );
+  }
+}
