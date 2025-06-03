9bc583cecfcc2d7584cf5beb910e0707cebb638a
Dinann <notxurd46@gmail.com>
2025-06-03 22:51:22 +0700

    feat(company-admin): add company admin dashboard header text

--- app/(admin)/dashboard/admin/page.tsx
+++ app/(admin)/dashboard/admin/page.tsx
@@ -1,7 +1,35 @@
+import { authOptions } from "@/app/api/auth/[...nextauth]/options";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 import React from "react";
 
-function SuperAdminCompanyPage() {
-  return <div>SuperAdminCompanyPage</div>;
+async function SuperAdminCompanyPage() {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    redirect("/");
+  }
+  return (
+    <section className="space-y-4">
+      {/* Company Admin Dashboard Header */}
+      <div>
+        <h1 className="text-lg font-bold md:text-xl lg:text-2xl">
+          Welcome back, {session.user.name} 👋
+        </h1>
+        <p className="text-muted-foreground">
+          Your central hub for system administration and company analytics.
+        </p>
+      </div>
+
+      {/* Statistics Card */}
+
+      {/* Department Overview */}
+      {/* Top Employees List */}
+      {/* Gender Diversity Chart */}
+      {/* Salaries Paid per Month Chart */}
+      {/* Employee Activity */}
+    </section>
+  );
 }
 
 export default SuperAdminCompanyPage;
