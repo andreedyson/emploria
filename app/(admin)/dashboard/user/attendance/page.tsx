149448b073bc217dcabd74cfe34e2a357f9589a1
Dinann <notxurd46@gmail.com>
2025-06-06 23:25:38 +0700

    feat(user): add all navigations page to user dashboard

---
+++ app/(admin)/dashboard/user/attendance/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function UserAttendancePage() {
+  return <div>UserAttendancePage</div>;
+}
+
+export default UserAttendancePage;
---
+++ app/(admin)/dashboard/user/salary/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function UserSalaryPage() {
+  return <div>UserSalaryPage</div>;
+}
+
+export default UserSalaryPage;
