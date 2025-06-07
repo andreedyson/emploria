5900533b03279819c27be84ab8c899826d6c8b37
Dinann <notxurd46@gmail.com>
2025-06-07 22:03:14 +0700

    feat(user): add user dashboard welcome header

--- app/(admin)/dashboard/user/page.tsx
+++ app/(admin)/dashboard/user/page.tsx
@@ -1,7 +1,28 @@
+import { authOptions } from "@/app/api/auth/[...nextauth]/options";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 import React from "react";
 
-function UserDashboardPage() {
-  return <div>UserDashboardPage</div>;
+async function UserDashboardPage() {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    redirect("/");
+  }
+
+  return (
+    <section className="space-y-4">
+      {/* User Dashboard Header */}
+      <div>
+        <h1 className="text-lg font-bold md:text-xl lg:text-2xl">
+          Welcome back, {session.user.name} 👋
+        </h1>
+        <p className="text-muted-foreground">
+          Manage your data and analyze your performance from one place.
+        </p>
+      </div>
+    </section>
+  );
 }
 
 export default UserDashboardPage;
