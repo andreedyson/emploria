fc6f23f5a4b721d3959b8c0e94ba873861217cd3
Dinann <notxurd46@gmail.com>
2025-06-28 10:52:10 +0700

    refactor(schema): removed and replaced some activity action enums

---
+++ prisma/migrations/20250628034807_remove_logout_from_activity_action/migration.sql
@@ -0,0 +1,14 @@
+/*
+  Warnings:
+
+  - The values [LOGOUT] on the enum `ActivityAction` will be removed. If these variants are still used in the database, this will fail.
+
+*/
+-- AlterEnum
+BEGIN;
+CREATE TYPE "ActivityAction_new" AS ENUM ('CREATE', 'UPDATE', 'DELETE', 'ACTIVATE', 'DEACTIVATE', 'LOGIN', 'RESET_PASSWORD', 'ASSIGN', 'UNASSIGN', 'APPROVE', 'REJECT', 'EXPORT');
+ALTER TABLE "Activity" ALTER COLUMN "action" TYPE "ActivityAction_new" USING ("action"::text::"ActivityAction_new");
+ALTER TYPE "ActivityAction" RENAME TO "ActivityAction_old";
+ALTER TYPE "ActivityAction_new" RENAME TO "ActivityAction";
+DROP TYPE "ActivityAction_old";
+COMMIT;
