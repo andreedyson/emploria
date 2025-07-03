b57a2248cc1a5410ff73b4d4b2653733a6636820
Dinann <notxurd46@gmail.com>
2025-07-03 19:26:54 +0700

    feat(super-admin): add the rest of loading skeletons to the super admin view

---
+++ app/(admin)/dashboard/super-admin/company/loading.tsx
@@ -0,0 +1,12 @@
+import TableSectionSkeletons from "@/components/skeletons/admin/table-section-skeletons";
+import React from "react";
+
+function SuperAdminCompanyPageLoading() {
+  return (
+    <>
+      <TableSectionSkeletons />
+    </>
+  );
+}
+
+export default SuperAdminCompanyPageLoading;
---
+++ app/(admin)/dashboard/super-admin/user/loading.tsx
@@ -0,0 +1,11 @@
+import TableSectionSkeletons from "@/components/skeletons/admin/table-section-skeletons";
+
+function SuperAdminUserPageLoading() {
+  return (
+    <>
+      <TableSectionSkeletons />
+    </>
+  );
+}
+
+export default SuperAdminUserPageLoading;
