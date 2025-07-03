01e4d212fbe6bce32f1f0f7338f0f6cf5e01f92a
Dinann <notxurd46@gmail.com>
2025-07-03 19:24:06 +0700

    feat(super-admin): add super admin dashboard skeletons loading

---
+++ app/(admin)/dashboard/super-admin/loading.tsx
@@ -0,0 +1,11 @@
+import SuperAdminDashboardSkeletons from "@/components/skeletons/super-admin/super-admin-dashboard-skeletons";
+
+function SuperAdminDashboardPageLoading() {
+  return (
+    <>
+      <SuperAdminDashboardSkeletons />
+    </>
+  );
+}
+
+export default SuperAdminDashboardPageLoading;
---
+++ components/skeletons/super-admin/super-admin-dashboard-skeletons.tsx
@@ -0,0 +1,28 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
+function SuperAdminDashboardSkeletons() {
+  return (
+    <>
+      {/* Welcome Header Skeleton */}
+      <div>
+        <Skeleton className="h-6 w-full max-w-[280px] md:max-w-[400px]" />
+        <Skeleton className="mt-2 h-3 w-full max-w-[180px] md:max-w-[480px]" />
+      </div>
+
+      {/* Stats Card Skeletons */}
+      <div className="mt-4 grid gap-4 md:grid-cols-2 lg:grid-cols-4">
+        {Array.from({ length: 4 }, (_, index) => (
+          <Skeleton className="h-[80px] w-full md:h-[120px]" key={index} />
+        ))}
+      </div>
+
+      {/* Users Per Company and Recently Added Skeletons */}
+      <div className="mt-4 grid grid-cols-1 gap-4 md:grid-cols-2">
+        <Skeleton className="h-[300px] w-full md:h-[350px]" />
+        <Skeleton className="h-[300px] w-full md:h-[350px]" />
+      </div>
+    </>
+  );
+}
+
+export default SuperAdminDashboardSkeletons;
