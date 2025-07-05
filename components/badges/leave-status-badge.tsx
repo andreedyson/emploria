d20dcc7c0f166bb1026ae486cc0f3754b59cd50a
Dinann <notxurd46@gmail.com>
2025-07-05 12:01:29 +0700

    style(user): add leave status badge styling for cancelled status

--- components/badges/leave-status-badge.tsx
+++ components/badges/leave-status-badge.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Badge } from "../ui/badge";
 import { LeaveStatus } from "@prisma/client";
-import { CheckCircle, CircleX, Loader } from "lucide-react";
+import { CheckCircle, CircleX, Loader, X } from "lucide-react";
 
 type LeaveStatusBadgeProps = {
   status: LeaveStatus;
@@ -15,7 +15,9 @@ function LeaveStatusBadge({ status }: LeaveStatusBadgeProps) {
         ? "text-green-600 bg-green-300/30 border-green-400"
         : status === "REJECTED"
           ? "text-red-600 bg-red-300/30 border-red-400"
-          : "text-gray-500 bg-gray-300/30 border-gray-400";
+          : status === "CANCELLED"
+            ? "text-amber-600 bg-amber-300/30 border-amber-400"
+            : "text-gray-500 bg-gray-300/30 border-gray-400";
   return (
     <Badge className={`${badgeStyle} rounded-full border font-semibold`}>
       {status === "PENDING" ? (
@@ -24,6 +26,8 @@ function LeaveStatusBadge({ status }: LeaveStatusBadgeProps) {
         <CheckCircle size={12} strokeWidth={3} />
       ) : status === "REJECTED" ? (
         <CircleX size={12} strokeWidth={3} />
+      ) : status === "CANCELLED" ? (
+        <X size={12} strokeWidth={3} />
       ) : (
         ""
       )}
